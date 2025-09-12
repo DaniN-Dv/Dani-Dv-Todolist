84a6d290e6d11f44424462feea61b5980fa42d8a
DaniN-Dv <danidanovasvalor@gmail.com>
2025-09-12 21:15:17 +0000

    terminado

--- src/js/components/Todolist.jsx
+++ src/js/components/Todolist.jsx
@@ -28,10 +28,9 @@ export const Todolist = () => {
         setmyTask(theTask)
     }
 
-    function deleteTask (index) {
-        console.log(`index = ${index}`)
+    const deleteTask = (id) => {
 
-        const newList = listTasks.filter((item) => item.index !== index)
+        const newList = listTasks.filter((item, index) => index !== id)
 
         setlistTasks(newList)
     }
@@ -40,28 +39,37 @@ export const Todolist = () => {
 
     return (
         <div>
+
             <h1 className="title">todos</h1>
+
             <div className="list">
                 <form onSubmit={handleSubmit} className="info">
 
-                    <input className="border border-0 form-control shadow-none " placeholder="What needs to be done" type="text" name="task" value={myTask.task} onChange={handleKeyDown} />
+                    <input className="border border-0 form-control shadow-none " 
+                    placeholder={`${listTasks.length == 0 ? 'No hay tareas, añadir tareas' : 'Añadir mas tareas'}`} 
+                    type="text" name="task" value={myTask.task} onChange={handleKeyDown} />
 
                     <ul className="p-0 m-0">
+
                         {listTasks.map((item, index) => (
                             <li key={index}>
-                                {/* <input type="checkbox" name="do" className="btn-check" value={myTask.do} onClick={(event) => setmyTask({...myTask, [event.target.name]: true})}/> */}
                                 <p className="m-0 py-2">{item.task}</p>
-                                <span onClick={() => deleteTask(listTasks.index)} className="text-danger">x</span>
+                                <span onClick={() => deleteTask(index)} className="text-danger">x</span>
                             </li>
                         ))}
+                        
                     </ul>
+
                     <p className="items">{`${listTasks.length} item left`}</p>
+
                 </form>
             </div>
+
             <div className="d-flex  flex-column align-items-center">
                 <div className="fondo1"></div>
                 <div className="fondo2"></div>
             </div>
+
         </div>
     )
 }
